c8ad2f4049515e586bf80d46d01a6a25c28295bd
madisonmcnealy <117848258+madisonmcnealy@users.noreply.github.com>
2025-11-28 23:21:49 -0500

    Update app.jsx

--- app.jsx
+++ app.jsx
@@ -1,14 +1,26 @@
-import DataDisplay from './pages/DataDisplay';
-import FormPage from './pages/FormPage';
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 
-function App() {
+import Home from './home.jsx'
+import DataDisplay from './DataDisplay.jsx'
+import FormPage from './FormPage.jsx'
+
+export default function App() {
   return (
-    <div>
-      <h1>Welcome to the Student Budget Tracker</h1>
-      <DataDisplay />
-      <FormPage />
-    </div>
-  );
-}
+    <Router>
+      <div style={{ fontFamily: 'Arial', padding: 20 }}>
+        <nav style={{ marginBottom: 20 }}>
+          <Link to="/" style={{ marginRight: 15 }}>Home</Link>
+          <Link to="/data" style={{ marginRight: 15 }}>Data Display</Link>
+          <Link to="/form">Form Page</Link>
+        </nav>
 
-export default App;
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/data" element={<DataDisplay />} />
+          <Route path="/form" element={<FormPage />} />
+        </Routes>
+      </div>
+    </Router>
+  )
+}
