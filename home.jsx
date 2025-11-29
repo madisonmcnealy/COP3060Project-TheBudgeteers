24e3939dabf24cb833230fca660a299fbd4fd3df
madisonmcnealy <117848258+madisonmcnealy@users.noreply.github.com>
2025-11-28 23:24:08 -0500

    Update home.jsx

--- home.jsx
+++ home.jsx
@@ -2,13 +2,12 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div>
-      <h1>Home / Dashboard</h1>
-      <p>Welcome! Use the links above to go to the Data Display and Form pages.</p>
-      <ul>
-        <li>Data Display: fetches items from the backend</li>
-        <li>Form Page: sends a new item to the backend</li>
-      </ul>
+    <div style={{ textAlign: 'center', marginTop: 50 }}>
+      <h1 style={{ fontSize: 32 }}>Student Budget Tracker</h1>
+      <p style={{ fontSize: 18, marginTop: 10 }}>
+        Use the navigation links above to view data or submit a new item.
+      </p>
     </div>
   )
 }
+
